6b01752f705ae5ed51ea484eb27ce58e753da97c
buoncubi <luca.buoncompagni@edu.unige.it>
2016-10-26 16:20:32 +0200

    Ultimated

---
+++ Doc/html/search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pointer_5farray_5fh_5f',['POINTER_ARRAY_H_',['../PointerArray_8cpp.html#ab22302e18165b53889d283f008d60f15',1,'PointerArray.cpp']]]
+];
---
+++ Doc/html/search/functions_10.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['_7edoctype',['~DocType',['../classdocs_1_1DocType.html#a931f25a0445c46eb7fba5c515124b728',1,'docs::DocType']]],
+  ['_7edocument',['~Document',['../classdocs_1_1Document.html#abcb73ac5ddf4e7b0c862220228b85b9e',1,'docs::Document']]],
+  ['_7epointerarray',['~PointerArray',['../classparray_1_1PointerArray.html#a3b59a3fe4f7e83d83231d9daf8210333',1,'parray::PointerArray']]],
+  ['_7espreadsheet',['~Spreadsheet',['../classdocs_1_1Spreadsheet.html#a5291016cd2e874e6ce3cf457723fc98c',1,'docs::Spreadsheet']]],
+  ['_7estringpointerarray',['~StringPointerArray',['../classparray_1_1StringPointerArray.html#a66e413fa7aedbfe9b45c87d8641ea36e',1,'parray::StringPointerArray']]],
+  ['_7ewebpage',['~WebPage',['../classdocs_1_1WebPage.html#a20af4ea1f7d488ae695b316a604ea6ad',1,'docs::WebPage']]]
+];
